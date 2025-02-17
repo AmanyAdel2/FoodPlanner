41fe02980f745c4c0e6323eec362c96304e32f70
Amany <amanyadel20242024@gmail.com>
2025-02-17 16:03:45 +0200

    "Meallist adabter"

---
+++ app/src/main/java/com/example/foodplanner/Models/MealList.java
@@ -0,0 +1,31 @@
+package com.example.foodplanner.Models;
+
+public class MealList {
+    public String strMeal;
+    public String strMealThumb;
+    public String idMeal;
+
+    public String getStrMeal() {
+        return strMeal;
+    }
+
+    public void setStrMeal(String strMeal) {
+        this.strMeal = strMeal;
+    }
+
+    public String getStrMealThumb() {
+        return strMealThumb;
+    }
+
+    public void setStrMealThumb(String strMealThumb) {
+        this.strMealThumb = strMealThumb;
+    }
+
+    public String getIdMeal() {
+        return idMeal;
+    }
+
+    public void setIdMeal(String idMeal) {
+        this.idMeal = idMeal;
+    }
+}
