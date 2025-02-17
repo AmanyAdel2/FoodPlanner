4be940c8b6033e40582ff114a1d66ad87c9e18ed
Amany <amanyadel20242024@gmail.com>
2025-02-17 14:54:35 +0200

    "country class"

---
+++ app/src/main/java/com/example/foodplanner/Models/Country.java
@@ -0,0 +1,15 @@
+package com.example.foodplanner.Models;
+
+import java.io.Serializable;
+
+public class Country implements Serializable {
+    public String strArea;
+
+    public String getStrArea() {
+        return strArea;
+    }
+
+    public void setStrArea(String strArea) {
+        this.strArea = strArea;
+    }
+}
